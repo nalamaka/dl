41c5915162bce0abaadf132ddc6789052c5fffc5
nalamaka <xuchangjie21@mails.ucas.ac.cn>
2026-08-28 16:01:40 +0800

    docs: add repository overview and Apache license

--- src_to_submit/exp2/utils.py
+++
@@ -1,7 +0,0 @@
-def progress_bar(batch_idx, total_batches, msg):
-    if batch_idx == total_batches - 1:
-        print(msg)
-
-
-def sparse_selection():
-    return
--- src_to_submit/exp6/run_one_click.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-setlocal
-powershell -ExecutionPolicy Bypass -File "%~dp0one_click_smoke.ps1"
-endlocal
